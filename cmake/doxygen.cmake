b2b7c38178a8637a72f88ec5bbe46ffd20c70f38
Ahmed Abdel Aal <eng.ahmedhussein89@gmail.com>
2022-12-10 18:23:08 +0200

    cmake: Add doxygen to the project

---
+++ cmake/doxygen.cmake
@@ -0,0 +1,11 @@
+if(ENABLE_DOXYGEN)
+  set(DOXYGEN_CALLER_GRAPH YES)
+  set(DOXYGEN_CALL_GRAPH   YES)
+  set(DOXYGEN_EXTRACT_ALL  YES)
+  find_package(Doxygen
+               REQUIRED dot)
+  doxygen_add_docs(doxygen-docs
+                   ${CMAKE_SOURCE_DIR}/src/lib/
+                   ${CMAKE_SOURCE_DIR}/src/lib_utility/
+                   ${CMAKE_SOURCE_DIR}/src/lib_gui)
+endif()
