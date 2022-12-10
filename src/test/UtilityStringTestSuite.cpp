0d4a047aa0ff5ca3af28c3585b36767dd5216c5d
Ahmed Abdel Aal <eng.ahmedhussein89@gmail.com>
2022-12-10 20:08:35 +0200

    test: add move coverage

--- src/test/UtilityStringTestSuite.cpp
+++ src/test/UtilityStringTestSuite.cpp
@@ -9,7 +9,7 @@ TEST_CASE("encodeToUtf8", "[utility][string]") {
 
 TEST_CASE("decodeFromUtf8", "[utility][string]") { REQUIRE(utility::decodeFromUtf8("HelloWorld!") == L"HelloWorld!"); }
 
-TEST_CASE("split with char delimiter", "[utility][string]") {
+TEST_CASE("split with char delimiter", "[utility][string]") { // NOLINT(readability-function-cognitive-complexity)
   SECTION("empty input") {
     const auto result = utility::split("", ',');
 
@@ -42,7 +42,7 @@ TEST_CASE("split with char delimiter", "[utility][string]") {
   }
 }
 
-TEST_CASE("split with string delimiter", "[utility][string]") {
+TEST_CASE("split with string delimiter", "[utility][string]") { // NOLINT(readability-function-cognitive-complexity)
   SECTION("goodcase") {
     std::deque<std::string> result = utility::split("A->B>C", "->");
 
@@ -94,13 +94,21 @@ TEST_CASE("split with string delimiter", "[utility][string]") {
   }
 }
 
-TEST_CASE("split with string delimiter to vector", "[utility][string]") {
+TEST_CASE("split with string delimiter to vector", "[utility][string]") { // NOLINT(readability-function-cognitive-complexity)
   SECTION("goodcase") {
     const auto result = utility::splitToVector("A->B>C", "->");
 
     REQUIRE(result.size() == 2);
     REQUIRE(result.at(0) == "A");
     REQUIRE(result.at(1) == "B>C");
+  }
+
+  SECTION("goodcase") {
+    const auto result = utility::splitToVector(L"A->B>C", L"->");
+
+    REQUIRE(result.size() == 2);
+    REQUIRE(result.at(0) == L"A");
+    REQUIRE(result.at(1) == L"B>C");
   }
 
   SECTION("empty string") {
@@ -127,6 +135,16 @@ TEST_CASE("split with string delimiter to vector", "[utility][string]") {
     REQUIRE(result.at(3) == "C");
   }
 
+  SECTION("ddelimiters next to each") {
+    const auto result = utility::splitToVector(L"A::B:C", ':');
+
+    REQUIRE(result.size() == 4);
+    REQUIRE(result.at(0) == L"A");
+    REQUIRE(result.at(1).empty());
+    REQUIRE(result.at(2) == L"B");
+    REQUIRE(result.at(3) == L"C");
+  }
+
   SECTION("delimiter at start") {
     const auto result = utility::splitToVector(":B:C", ':');
 
@@ -146,7 +164,7 @@ TEST_CASE("split with string delimiter to vector", "[utility][string]") {
   }
 }
 
-TEST_CASE("join with char delimiter", "[utility][string]") {
+TEST_CASE("join with char delimiter", "[utility][string]") { // NOLINT(readability-function-cognitive-complexity)
   SECTION("goodcase") {
     std::deque<std::string> list;
     list.emplace_back("A");
@@ -154,6 +172,26 @@ TEST_CASE("join with char delimiter", "[utility][string]") {
     list.emplace_back("C");
 
     std::string result = utility::join(list, ',');
+    REQUIRE(result == "A,B,C");
+  }
+
+  SECTION("goodcase vector") {
+    std::vector<std::string> vec;
+    vec.emplace_back("A");
+    vec.emplace_back("B");
+    vec.emplace_back("C");
+
+    std::string result = utility::join(vec, ',');
+    REQUIRE(result == "A,B,C");
+  }
+
+  SECTION("goodcase vector with string") {
+    std::vector<std::string> vec;
+    vec.emplace_back("A");
+    vec.emplace_back("B");
+    vec.emplace_back("C");
+
+    std::string result = utility::join(vec, ",");
     REQUIRE(result == "A,B,C");
   }
 
@@ -186,8 +224,8 @@ TEST_CASE("join with string delimiter", "[utility][string]") {
   }
 }
 
-TEST_CASE("tokenize with string", "[utility][string]") {
-  SECTION("goodcase") {
+TEST_CASE("tokenize with string", "[utility][string]") { // NOLINT(readability-function-cognitive-complexity)
+  SECTION("goodcase with string") {
     std::deque<std::string> result = utility::tokenize("A->B->C", "->");
 
     REQUIRE(result.size() == 5);
@@ -195,6 +233,17 @@ TEST_CASE("tokenize with string", "[utility][string]") {
     REQUIRE(result.at(1) == "->");
     REQUIRE(result.at(2) == "B");
     REQUIRE(result.at(3) == "->");
+    REQUIRE(result.at(4) == "C");
+  }
+
+  SECTION("goodcase") {
+    std::deque<std::string> result = utility::tokenize("A,B,C", ',');
+
+    REQUIRE(result.size() == 5);
+    REQUIRE(result.at(0) == "A");
+    REQUIRE(result.at(1) == ",");
+    REQUIRE(result.at(2) == "B");
+    REQUIRE(result.at(3) == ",");
     REQUIRE(result.at(4) == "C");
   }
 
@@ -216,6 +265,20 @@ TEST_CASE("tokenize with string", "[utility][string]") {
 
   SECTION("deque") {
     std::deque<std::string> result = utility::tokenize("A->B=C->D", "->");
+    result = utility::tokenize(result, '=');
+
+    REQUIRE(result.size() == 7);
+    REQUIRE(result.at(0) == "A");
+    REQUIRE(result.at(1) == "->");
+    REQUIRE(result.at(2) == "B");
+    REQUIRE(result.at(3) == "=");
+    REQUIRE(result.at(4) == "C");
+    REQUIRE(result.at(5) == "->");
+    REQUIRE(result.at(6) == "D");
+  }
+
+  SECTION("deque with string") {
+    std::deque<std::string> result = utility::tokenize("A->B=C->D", "->");
     result = utility::tokenize(result, "=");
 
     REQUIRE(result.size() == 7);
@@ -229,25 +292,39 @@ TEST_CASE("tokenize with string", "[utility][string]") {
   }
 }
 
-TEST_CASE("substr", "[utility][string]") {
+TEST_CASE("substr", "[utility][string]") { // NOLINT(readability-function-cognitive-complexity)
   SECTION("before first with single delimiter occurrence") {
     REQUIRE(utility::substrBeforeFirst("foo bar", ' ') == "foo");
+  }
+
+  SECTION("before first with single delimiter occurrence string") {
+    REQUIRE(utility::substrBeforeFirst("foo bar", " ") == "foo");
   }
 
   SECTION("before first with multiple delimiter occurrences") {
     REQUIRE(utility::substrBeforeFirst("foo bar foo", ' ') == "foo");
   }
 
+  SECTION("before first with no delimiter occurrence string") {
+    REQUIRE(utility::substrBeforeFirst("foobar", " ") == "foobar");
+  }
+
   SECTION("before first with no delimiter occurrence") {
     REQUIRE(utility::substrBeforeFirst("foobar", ' ') == "foobar");
   }
 
-  SECTION("before first with delimiter at start") { REQUIRE(utility::substrBeforeFirst(" foobar", ' ') == ""); }
+  SECTION("before first with delimiter at start") { REQUIRE(utility::substrBeforeFirst(" foobar", ' ').empty()); }
 
   SECTION("before first with delimiter at end") { REQUIRE(utility::substrBeforeFirst("foobar ", ' ') == "foobar"); }
 
+  SECTION("missing delimiter") {
+    REQUIRE(utility::substrBeforeLast("foo", ' ') == "foo");
+    REQUIRE(utility::substrBeforeLast(L"foo", L' ') == L"foo");
+  }
+
   SECTION("before last with single delimiter occurrence") {
     REQUIRE(utility::substrBeforeLast("foo bar", ' ') == "foo");
+    REQUIRE(utility::substrBeforeLast(L"foo bar", L' ') == L"foo");
   }
 
   SECTION("before last with multiple delimiter occurrences") {
@@ -256,7 +333,7 @@ TEST_CASE("substr", "[utility][string]") {
 
   SECTION("before last with no delimiter occurrence") { REQUIRE(utility::substrBeforeLast("foobar", ' ') == "foobar"); }
 
-  SECTION("before last with delimiter at start") { REQUIRE(utility::substrBeforeLast(" foobar", ' ') == ""); }
+  SECTION("before last with delimiter at start") { REQUIRE(utility::substrBeforeLast(" foobar", ' ').empty()); }
 
   SECTION("before last with delimiter at end") { REQUIRE(utility::substrBeforeLast("foobar ", ' ') == "foobar"); }
 
@@ -270,10 +347,28 @@ TEST_CASE("substr", "[utility][string]") {
 
   SECTION("after with delimiter at start") { REQUIRE(utility::substrAfter(" foobar", ' ') == "foobar"); }
 
-  SECTION("after with delimiter at end") { REQUIRE(utility::substrAfter("foobar ", ' ') == ""); }
+  SECTION("after with delimiter at end") { REQUIRE(utility::substrAfter("foobar ", ' ').empty()); }
+
+  SECTION("missing delimiter") {
+    REQUIRE(utility::substrAfterLast(L"somthing", L' ') == L"somthing");
+  }
+
+  SECTION("After last delimiter") {
+    REQUIRE(utility::substrAfterLast(L"somthing is there", L' ') == L"there");
+  }
+
+  SECTION("substr After") {
+    REQUIRE(utility::substrAfter("somthing is there", ' ') == "is there");
+    REQUIRE(utility::substrAfter("somthing is there", " ") == "is there");
+  }
+
+  SECTION("missing delimiter") {
+    REQUIRE(utility::substrAfter("somthing", ' ') == "somthing");
+    REQUIRE(utility::substrAfter("somthing", " ") == "somthing");
+  }
 }
 
-TEST_CASE("isPrefix", "[utility][string]") {
+TEST_CASE("isPrefix", "[utility][string]") { // NOLINT(readability-function-cognitive-complexity)
   SECTION("empty string is detected as prefix of any other string") {
     const std::string foo = "foo";
 
@@ -306,6 +401,20 @@ TEST_CASE("to lower case", "[utility][string]") {
   REQUIRE("foobar" == utility::toLowerCase("FooBar"));
   REQUIRE("foobar" == utility::toLowerCase("FOOBAR"));
   REQUIRE("foobar" == utility::toLowerCase("foobar"));
+
+  REQUIRE(L"foobar" == utility::toLowerCase(L"FooBar"));
+  REQUIRE(L"foobar" == utility::toLowerCase(L"FOOBAR"));
+  REQUIRE(L"foobar" == utility::toLowerCase(L"foobar"));
+}
+
+TEST_CASE("to upper case", "[utility][string]") {
+  REQUIRE("FOOBAR" == utility::toUpperCase("FooBar"));
+  REQUIRE("FOOBAR" == utility::toUpperCase("FOOBAR"));
+  REQUIRE("FOOBAR" == utility::toUpperCase("foobar"));
+
+  REQUIRE(L"FOOBAR" == utility::toUpperCase(L"FooBar"));
+  REQUIRE(L"FOOBAR" == utility::toUpperCase(L"FOOBAR"));
+  REQUIRE(L"FOOBAR" == utility::toUpperCase(L"foobar"));
 }
 
 TEST_CASE("equals case insensitive", "[utility][string]") {
@@ -331,17 +440,17 @@ TEST_CASE("equals case insensitive", "[utility][string]") {
   }
 }
 
-TEST_CASE("replace", "[utility][string]") {
+TEST_CASE("replace", "[utility][string]") { // NOLINT(readability-function-cognitive-complexity)
   REQUIRE("fubar" == utility::replace("foobar", "oo", "u"));
   REQUIRE("fuuuubar" == utility::replace("foobar", "o", "uu"));
   REQUIRE("bar" == utility::replace("foobar", "foo", ""));
   REQUIRE("foobar" == utility::replace("foobar", "", "i"));
   REQUIRE("foobar" == utility::replace("foobar", "", ""));
-  REQUIRE("" == utility::replace("", "foo", "bar"));
+  REQUIRE(utility::replace("", "foo", "bar").empty());
   REQUIRE("foobar" == utility::replace("foobar", "ba", "ba"));
 }
 
-TEST_CASE("caseInsensitiveLess", "[utility][string]") {
+TEST_CASE("caseInsensitiveLess", "[utility][string]") { // NOLINT(readability-function-cognitive-complexity)
   SECTION("should return false when comparing empty wstrings") {
     REQUIRE_FALSE(utility::caseInsensitiveLess(L"", L""));
   }
@@ -406,5 +515,59 @@ TEST_CASE("caseInsensitiveLess", "[utility][string]") {
       "should return false when after lower casing both wstrings, second is "
       "prefix of first") {
     REQUIRE_FALSE(utility::caseInsensitiveLess(L"ab_cD!E", L"aB_cd!"));
+  }
+}
+
+TEST_CASE("trim", "[utility][string]") { // NOLINT(readability-function-cognitive-complexity)
+  SECTION("goodcase") {
+    REQUIRE_THAT(utility::trim("   HelloWorld!   "), Catch::Equals("HelloWorld!"));
+    REQUIRE(utility::trim(L"   HelloWorld!   ") == L"HelloWorld!");
+  }
+
+  SECTION("space at front") {
+    REQUIRE_THAT(utility::trim("   HelloWorld!"), Catch::Equals("HelloWorld!"));
+    REQUIRE(utility::trim(L"   HelloWorld!") == L"HelloWorld!");
+  }
+
+  SECTION("space at back") {
+    REQUIRE_THAT(utility::trim("HelloWorld!  "), Catch::Equals("HelloWorld!"));
+    REQUIRE(utility::trim(L"HelloWorld!  ") == L"HelloWorld!");
+  }
+
+  SECTION("space in middle") {
+    REQUIRE_THAT(utility::trim("Hello   World!"), Catch::Equals("Hello   World!"));
+    REQUIRE(utility::trim(L"Hello   World!") == L"Hello   World!");
+  }
+
+  SECTION("spaces") {
+    REQUIRE(utility::trim("   ").empty());
+    REQUIRE(utility::trim(L"   ").empty());
+  }
+}
+
+TEST_CASE("convertWhiteSpacesToSingleSpaces", "[utility][string]") {
+  SECTION("goodcase") {
+    REQUIRE(utility::convertWhiteSpacesToSingleSpaces(L"Hello   World!") == L"Hello World!");
+  }
+
+  SECTION("spaces only") {
+    REQUIRE(utility::convertWhiteSpacesToSingleSpaces(L"   ").empty());
+  }
+}
+
+TEST_CASE("elide", "[utility][string]") {
+  SECTION("goodcase Left") {
+    REQUIRE(utility::elide("HelloWorld!", utility::ElideMode::ELIDE_LEFT, 3) == "...World!");
+    REQUIRE(utility::elide(L"HelloWorld!", utility::ElideMode::ELIDE_LEFT, 3) == L"...World!");
+  }
+
+  SECTION("goodcase Middle") {
+    REQUIRE(utility::elide("HelloWorld!", utility::ElideMode::ELIDE_MIDDLE, 10) == "Hell...ld!");
+    REQUIRE(utility::elide(L"HelloWorld!", utility::ElideMode::ELIDE_MIDDLE, 10) == L"Hell...ld!");
+  }
+
+  SECTION("goodcase Right") {
+    REQUIRE(utility::elide("HelloWorld!", utility::ElideMode::ELIDE_RIGHT, 0) == "HelloWorld!...");
+    REQUIRE(utility::elide(L"HelloWorld!", utility::ElideMode::ELIDE_RIGHT, 0) == L"HelloWorld!...");
   }
 }
