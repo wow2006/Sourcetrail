ae14b44e7612dc1994c06659f89a8f24f94a5c5f
Ahmed Hussein <eng.ahmedhussein89@gmail.com>
2022-12-17 16:06:48 +0200

    test: Add a functionality test
    ticket: SOUR-13

--- src/functionality/indexerFuncTest.py
+++ src/functionality/indexerFuncTest.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python3
+from argparse import ArgumentParser
+import os
+import subprocess
 
-print("HelloWorld!")
+def main(indexer_path):
+    """
+    1. check the exectuable exists
+    2. run the exectuable with corrent argments
+    3. check the process exit value
+    4. check the process output
+    """
+    #
+    assert os.path.exists(indexer_path)
+    #
+    process = subprocess.run(indexer_path, check=True)
+    assert process.returncode == 0
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("process_path", type=str)
+    args = parser.parse_args()
+    main(args.process_path)
