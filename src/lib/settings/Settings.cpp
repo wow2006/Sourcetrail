71b4749c87912aa0f19f017d7092084f4053e521
Ahmed Abdel Aal <eng.ahmedhussein89@gmail.com>
2022-12-27 08:32:54 +0200

    src: Refactor Settings
    ticket: SOUR-15

--- src/lib/settings/Settings.cpp
+++ src/lib/settings/Settings.cpp
@@ -1,151 +1,124 @@
 #include "Settings.h"
 
+#include "FilePath.h"
 #include "TextAccess.h"
 #include "logging.h"
-#include "utility.h"
+
+Settings::Settings() {
+  clear();
+}
 
 Settings::Settings(const Settings& other)
-	: m_config(other.m_config->createCopy()), m_filePath(other.m_filePath)
-{
+    : m_config(other.m_config->createCopy()), m_filePath(other.m_filePath) {}
+
+Settings& Settings::operator=(const Settings& other) {
+  if(&other != this) {
+    m_filePath = other.m_filePath;
+    m_config = other.m_config->createCopy();
+  }
+
+  return *this;
 }
 
-Settings& Settings::operator=(const Settings& other)
-{
-	if (&other != this)
-	{
-		m_filePath = other.m_filePath;
-		m_config = other.m_config->createCopy();
-	}
+Settings::Settings(Settings&& other) noexcept = default;
 
-	return *this;
+Settings& Settings::operator=(Settings&& other) noexcept = default;
+
+Settings::~Settings() = default;
+
+bool Settings::load(const FilePath& filePath, bool readOnly) {
+  m_readOnly = readOnly;
+
+  if(filePath.exists()) {
+    m_config = ConfigManager::createAndLoad(TextAccess::createFromFile(filePath));
+    m_filePath = filePath;
+    return true;
+  }
+  clear();
+  m_filePath = filePath;
+  LOG_WARNING(L"File for Settings not found: " + filePath.wstr());
+  return false;
 }
 
-Settings::~Settings() {}
+bool Settings::loadFromString(const std::string& text, bool readOnly) {
+  m_readOnly = readOnly;
 
-bool Settings::load(const FilePath& filePath, bool readOnly)
-{
-	m_readOnly = readOnly;
-
-	if (filePath.exists())
-	{
-		m_config = ConfigManager::createAndLoad(TextAccess::createFromFile(filePath));
-		m_filePath = filePath;
-		return true;
-	}
-	else
-	{
-		clear();
-		m_filePath = filePath;
-		LOG_WARNING(L"File for Settings not found: " + filePath.wstr());
-		return false;
-	}
+  m_config = ConfigManager::createAndLoad(TextAccess::createFromString(text));
+  m_filePath = FilePath();
+  return true;
 }
 
-bool Settings::loadFromString(const std::string& text, bool readOnly)
-{
-	m_readOnly = readOnly;
+bool Settings::save() {
+  if(m_readOnly) {
+    return false;
+  }
 
-	m_config = ConfigManager::createAndLoad(TextAccess::createFromString(text));
-	m_filePath = FilePath();
-	return true;
+  bool success = false;
+  if(m_config && !m_filePath.empty()) {
+    success = m_config->save(m_filePath.str());
+  }
+
+  if(!success) {
+    LOG_WARNING(L"Settings were not saved: " + m_filePath.wstr());
+  }
+
+  return success;
 }
 
-bool Settings::save()
-{
-	if (m_readOnly)
-	{
-		return false;
-	}
+bool Settings::save(const FilePath& filePath) {
+  setFilePath(filePath);
 
-	bool success = false;
-	if (m_config.get() && !m_filePath.empty())
-	{
-		success = m_config->save(m_filePath.str());
-	}
-
-	if (!success)
-	{
-		LOG_WARNING(L"Settings were not saved: " + m_filePath.wstr());
-	}
-
-	return success;
+  return save();
 }
 
-bool Settings::save(const FilePath& filePath)
-{
-	setFilePath(filePath);
-
-	return save();
+void Settings::clear() {
+  m_config = ConfigManager::createEmpty();
+  m_filePath = FilePath();
 }
 
-void Settings::clear()
-{
-	m_config = ConfigManager::createEmpty();
-	m_filePath = FilePath();
+const FilePath& Settings::getFilePath() const {
+  return m_filePath;
 }
 
-const FilePath& Settings::getFilePath() const
-{
-	return m_filePath;
+size_t Settings::getVersion() const {
+  return static_cast<size_t>(getValue<int>("version", 0));
 }
 
-size_t Settings::getVersion() const
-{
-	return getValue<int>("version", 0);
+void Settings::setVersion(size_t version) {
+  setValue<int>("version", static_cast<int>(version));
 }
 
-void Settings::setVersion(size_t version)
-{
-	setValue<int>("version", static_cast<int>(version));
+void Settings::setFilePath(const FilePath& filePath) {
+  m_filePath = filePath;
 }
 
-Settings::Settings()
-{
-	clear();
+std::vector<FilePath> Settings::getPathValues(const std::string& key) const {
+  std::vector<FilePath> paths;
+  std::ranges::transform(getValues<std::wstring>(key, {}),
+                         std::back_inserter(paths),
+                         [](const auto& path) -> FilePath { return FilePath {path}; });
+  return paths;
 }
 
-void Settings::setFilePath(const FilePath& filePath)
-{
-	m_filePath = filePath;
+bool Settings::setPathValues(const std::string& key, const std::vector<FilePath>& paths) {
+  std::vector<std::wstring> values;
+  std::ranges::transform(
+      paths, std::back_inserter(values), [](const auto& path) { return path.wstr(); });
+  return setValues(key, values);
 }
 
-std::vector<FilePath> Settings::getPathValues(const std::string& key) const
-{
-	std::vector<FilePath> paths;
-	for (const std::wstring& value: getValues<std::wstring>(key, {}))
-	{
-		paths.push_back(FilePath(value));
-	}
-	return paths;
+bool Settings::isValueDefined(const std::string& key) const {
+  return m_config->isValueDefined(key);
 }
 
-bool Settings::setPathValues(const std::string& key, const std::vector<FilePath>& paths)
-{
-	std::vector<std::wstring> values;
-	for (const FilePath& path: paths)
-	{
-		values.push_back(path.wstr());
-	}
-
-	return setValues(key, values);
+void Settings::removeValues(const std::string& key) {
+  m_config->removeValues(key);
 }
 
-bool Settings::isValueDefined(const std::string& key) const
-{
-	return m_config->isValueDefined(key);
+void Settings::enableWarnings() const {
+  m_config->setWarnOnEmptyKey(true);
 }
 
-void Settings::removeValues(const std::string& key)
-{
-	m_config->removeValues(key);
-}
-
-void Settings::enableWarnings() const
-{
-	m_config->setWarnOnEmptyKey(true);
-}
-
-void Settings::disableWarnings() const
-{
-	m_config->setWarnOnEmptyKey(false);
+void Settings::disableWarnings() const {
+  m_config->setWarnOnEmptyKey(false);
 }
--- src/lib/settings/Settings.h
+++ src/lib/settings/Settings.h
@@ -1,119 +1,107 @@
-#ifndef SETTINGS_H
-#define SETTINGS_H
-
+#pragma once
+// STL
 #include <memory>
 #include <string>
 #include <vector>
-
+// Internal
 #include "ConfigManager.h"
 #include "FilePath.h"
 
 class SettingsMigration;
 
-class Settings
-{
+class Settings {
 public:
-	Settings(const Settings& other);
-	Settings& operator=(const Settings& other);
-	virtual ~Settings();
+  Settings(const Settings& other);
+  Settings& operator=(const Settings& other);
+  Settings(Settings&& other) noexcept;
+  Settings& operator=(Settings&& other) noexcept;
+  virtual ~Settings();
 
-	bool load(const FilePath& filePath, bool readOnly = false);
-	bool loadFromString(const std::string& text, bool readOnly = false);
-	bool save();
-	bool save(const FilePath& filePath);
+  bool load(const FilePath& filePath, bool readOnly = false);
+  bool loadFromString(const std::string& text, bool readOnly = false);
+  bool save();
+  bool save(const FilePath& filePath);
 
-	void clear();
+  void clear();
 
-	virtual const FilePath& getFilePath() const;
+  virtual const FilePath& getFilePath() const;
 
-	size_t getVersion() const;
-	void setVersion(size_t version);
+  size_t getVersion() const;
+  void setVersion(size_t version);
 
 protected:
-	Settings();
+  Settings();
 
-	void setFilePath(const FilePath& filePath);
+  void setFilePath(const FilePath& filePath);
 
-	template <typename T>
-	T getValue(const std::string& key, T defaultValue) const;
+  template <typename T>
+  T getValue(const std::string& key, T defaultValue) const;
 
-	template <typename T>
-	std::vector<T> getValues(const std::string& key, std::vector<T> defaultValues) const;
+  template <typename T>
+  std::vector<T> getValues(const std::string& key, std::vector<T> defaultValues) const;
 
-	std::vector<FilePath> getPathValues(const std::string& key) const;
+  std::vector<FilePath> getPathValues(const std::string& key) const;
 
-	template <typename T>
-	bool setValue(const std::string& key, T value);
+  template <typename T>
+  bool setValue(const std::string& key, T value);
 
-	template <typename T>
-	bool setValues(const std::string& key, std::vector<T> values);
+  template <typename T>
+  bool setValues(const std::string& key, std::vector<T> values);
 
-	bool setPathValues(const std::string& key, const std::vector<FilePath>& paths);
+  bool setPathValues(const std::string& key, const std::vector<FilePath>& paths);
 
-	bool isValueDefined(const std::string& key) const;
+  bool isValueDefined(const std::string& key) const;
 
-	void removeValues(const std::string& key);
+  void removeValues(const std::string& key);
 
-	void enableWarnings() const;
-	void disableWarnings() const;
+  void enableWarnings() const;
+  void disableWarnings() const;
 
-	std::shared_ptr<ConfigManager> m_config;
+  std::shared_ptr<ConfigManager> m_config;
 
 private:
-	FilePath m_filePath;
-	bool m_readOnly = false;
+  FilePath m_filePath;
+  bool m_readOnly = false;
 
-	friend SettingsMigration;
+  friend SettingsMigration;
 };
 
 template <typename T>
-T Settings::getValue(const std::string& key, T defaultValue) const
-{
-	if (m_config)
-	{
-		T value;
-		if (m_config->getValue(key, value))
-		{
-			return value;
-		}
-	}
-	return defaultValue;
+T Settings::getValue(const std::string& key, T defaultValue) const {
+  if(m_config) {
+    T value;
+    if(m_config->getValue(key, value)) {
+      return value;
+    }
+  }
+  return defaultValue;
 }
 
 template <typename T>
-std::vector<T> Settings::getValues(const std::string& key, std::vector<T> defaultValues) const
-{
-	if (m_config)
-	{
-		std::vector<T> values;
-		if (m_config->getValues(key, values))
-		{
-			return values;
-		}
-	}
-	return defaultValues;
+std::vector<T> Settings::getValues(const std::string& key, std::vector<T> defaultValues) const {
+  if(m_config) {
+    std::vector<T> values;
+    if(m_config->getValues(key, values)) {
+      return values;
+    }
+  }
+  return defaultValues;
 }
 
 template <typename T>
-bool Settings::setValue(const std::string& key, T value)
-{
-	if (m_config)
-	{
-		m_config->setValue(key, value);
-		return true;
-	}
-	return false;
+bool Settings::setValue(const std::string& key, T value) {
+  if(m_config) {
+    m_config->setValue(key, value);
+    return true;
+  }
+  return false;
 }
 
 template <typename T>
-bool Settings::setValues(const std::string& key, std::vector<T> values)
-{
-	if (m_config)
-	{
-		m_config->setValues(key, values);
-		return true;
-	}
-	return false;
+bool Settings::setValues(const std::string& key, std::vector<T> values) {
+  if(m_config) {
+    m_config->setValues(key, values);
+    return true;
+  }
+  return false;
 }
-
-#endif	  // SETTINGS_H
