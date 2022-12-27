f1f32f1b736f90ff53f44ab5197af878775d5b37
Ahmed Abdel Aal <eng.ahmedhussein89@gmail.com>
2022-12-27 08:00:49 +0200

    src: Refactor FilePathFilter
    ticket: SOUR-15

--- src/lib/utility/file/FilePathFilter.cpp
+++ src/lib/utility/file/FilePathFilter.cpp
@@ -1,103 +1,96 @@
 #include "FilePathFilter.h"
 
 FilePathFilter::FilePathFilter(const std::wstring& filterString)
-	: m_filterString(filterString), m_filterRegex(convertFilterStringToRegex(filterString))
-{
+    : m_filterString(filterString), m_filterRegex(convertFilterStringToRegex(filterString)) {}
+
+std::wstring FilePathFilter::wstr() const {
+  return m_filterString;
 }
 
-std::wstring FilePathFilter::wstr() const
-{
-	return m_filterString;
+bool FilePathFilter::isMatching(const FilePath& filePath) const {
+  return isMatching(filePath.wstr());
 }
 
-bool FilePathFilter::isMatching(const FilePath& filePath) const
-{
-	return isMatching(filePath.wstr());
+bool FilePathFilter::isMatching(const std::wstring& fileStr) const {
+  return std::regex_match(fileStr, m_filterRegex);
 }
 
-bool FilePathFilter::isMatching(const std::wstring& fileStr) const
-{
-	return std::regex_match(fileStr, m_filterRegex);
+bool FilePathFilter::operator<(const FilePathFilter& other) const {
+  return m_filterString.compare(other.m_filterString) < 0;
 }
 
-bool FilePathFilter::operator<(const FilePathFilter& other) const
-{
-	return m_filterString.compare(other.m_filterString) < 0;
-}
+std::wregex FilePathFilter::convertFilterStringToRegex(const std::wstring& filterString) {
+  std::wstring regexFilterString = filterString;
 
-std::wregex FilePathFilter::convertFilterStringToRegex(const std::wstring& filterString)
-{
-	std::wstring regexFilterString = filterString;
+  {
+    std::wregex regex(L"[\\\\/]");
+    regexFilterString = std::regex_replace(regexFilterString, regex, L"[\\\\/]");
+  }
 
-	{
-		std::wregex regex(L"[\\\\/]");
-		regexFilterString = std::regex_replace(regexFilterString, regex, L"[\\\\/]");
-	}
+  {
+    std::wregex regex(L"([^\\\\])([^/])([\\]])");
+    regexFilterString = std::regex_replace(regexFilterString, regex, L"$1$2[\\]]");
+  }
 
-	{
-		std::wregex regex(L"([^\\\\])([^/])([\\]])");
-		regexFilterString = std::regex_replace(regexFilterString, regex, L"$1$2[\\]]");
-	}
+  {
+    std::wregex regex(L"([\\[])([^\\\\])");
+    regexFilterString = std::regex_replace(regexFilterString, regex, L"[\\[]$2");
+  }
 
-	{
-		std::wregex regex(L"([\\[])([^\\\\])");
-		regexFilterString = std::regex_replace(regexFilterString, regex, L"[\\[]$2");
-	}
+  {
+    std::wregex regex(L"[\\(]");
+    regexFilterString = std::regex_replace(regexFilterString, regex, L"[\\(]");
+  }
 
-	{
-		std::wregex regex(L"[\\(]");
-		regexFilterString = std::regex_replace(regexFilterString, regex, L"[\\(]");
-	}
+  {
+    std::wregex regex(L"[\\)]");
+    regexFilterString = std::regex_replace(regexFilterString, regex, L"[\\)]");
+  }
 
-	{
-		std::wregex regex(L"[\\)]");
-		regexFilterString = std::regex_replace(regexFilterString, regex, L"[\\)]");
-	}
+  {
+    std::wregex regex(L"[\\{]");
+    regexFilterString = std::regex_replace(regexFilterString, regex, L"[\\{]");
+  }
 
-	{
-		std::wregex regex(L"[\\{]");
-		regexFilterString = std::regex_replace(regexFilterString, regex, L"[\\{]");
-	}
+  {
+    std::wregex regex(L"[\\}]");
+    regexFilterString = std::regex_replace(regexFilterString, regex, L"[\\}]");
+  }
 
-	{
-		std::wregex regex(L"[\\}]");
-		regexFilterString = std::regex_replace(regexFilterString, regex, L"[\\}]");
-	}
+  {
+    std::wregex regex(L"[\\+]");
+    regexFilterString = std::regex_replace(regexFilterString, regex, L"[\\+]");
+  }
 
-	{
-		std::wregex regex(L"[\\+]");
-		regexFilterString = std::regex_replace(regexFilterString, regex, L"[\\+]");
-	}
+  {
+    std::wregex regex(L"[\\-]");
+    regexFilterString = std::regex_replace(regexFilterString, regex, L"[\\-]");
+  }
 
-	{
-		std::wregex regex(L"[\\-]");
-		regexFilterString = std::regex_replace(regexFilterString, regex, L"[\\-]");
-	}
+  {
+    std::wregex regex(L"[\\$]");
+    regexFilterString = std::regex_replace(regexFilterString, regex, L"[\\$]");
+  }
 
-	{
-		std::wregex regex(L"[\\$]");
-		regexFilterString = std::regex_replace(regexFilterString, regex, L"[\\$]");
-	}
+  {
+    std::wregex regex(L"[\\.]");
+    regexFilterString = std::regex_replace(regexFilterString, regex, L"[\\.]");
+  }
 
-	{
-		std::wregex regex(L"[\\.]");
-		regexFilterString = std::regex_replace(regexFilterString, regex, L"[\\.]");
-	}
+  {
+    std::wregex regex(L"[\\^]");
+    regexFilterString = std::regex_replace(regexFilterString, regex, L"[\\^]");
+  }
 
-	{
-		std::wregex regex(L"[\\^]");
-		regexFilterString = std::regex_replace(regexFilterString, regex, L"[\\^]");
-	}
+  {
+    std::wregex regex(L"[\\*][\\*]");
+    regexFilterString = std::regex_replace(regexFilterString, regex, L".{0,}");
+  }
 
-	{
-		std::wregex regex(L"[\\*][\\*]");
-		regexFilterString = std::regex_replace(regexFilterString, regex, L".{0,}");
-	}
+  {
+    std::wregex regex(L"[\\*]");
+    regexFilterString = std::regex_replace(regexFilterString, regex, L"[^\\\\/]*");
+  }
 
-	{
-		std::wregex regex(L"[\\*]");
-		regexFilterString = std::regex_replace(regexFilterString, regex, L"[^\\\\/]*");
-	}
-
-	return std::wregex(regexFilterString, std::regex::optimize);
+  return std::wregex(regexFilterString, std::regex::optimize);
 }
--- src/lib/utility/file/FilePathFilter.h
+++ src/lib/utility/file/FilePathFilter.h
@@ -1,47 +1,40 @@
-#ifndef FILE_PATH_FILTER_H
-#define FILE_PATH_FILTER_H
+#pragma once
 
 #include <regex>
 #include <string>
 
 #include "FilePath.h"
 
-class FilePathFilter
-{
+class FilePathFilter {
 public:
-	template<typename ContainerType>
-	static bool areMatching(const ContainerType& filters, const FilePath& filePath);
+  template <typename ContainerType>
+  static bool areMatching(const ContainerType& filters, const FilePath& filePath);
 
-	explicit FilePathFilter(const std::wstring& filterString);
+  explicit FilePathFilter(const std::wstring& filterString);
 
-	std::wstring wstr() const;
+  std::wstring wstr() const;
 
-	bool isMatching(const FilePath& filePath) const;
-	bool isMatching(const std::wstring& fileStr) const;
+  bool isMatching(const FilePath& filePath) const;
+  bool isMatching(const std::wstring& fileStr) const;
 
-	bool operator<(const FilePathFilter& other) const;
+  bool operator<(const FilePathFilter& other) const;
 
 private:
-	static std::wregex convertFilterStringToRegex(const std::wstring& filterString);
+  static std::wregex convertFilterStringToRegex(const std::wstring& filterString);
 
-	std::wstring m_filterString;
-	std::wregex m_filterRegex;
+  std::wstring m_filterString;
+  std::wregex m_filterRegex;
 };
 
-template<typename ContainerType>
-bool FilePathFilter::areMatching(const ContainerType& filters, const FilePath& filePath)
-{
-	const std::wstring fileStr = filePath.wstr();
+template <typename ContainerType>
+bool FilePathFilter::areMatching(const ContainerType& filters, const FilePath& filePath) {
+  const std::wstring fileStr = filePath.wstr();
 
-	for (const FilePathFilter& filter: filters)
-	{
-		if (filter.isMatching(fileStr))
-		{
-			return true;
-		}
-	}
+  for(const FilePathFilter& filter: filters) {
+    if(filter.isMatching(fileStr)) {
+      return true;
+    }
+  }
 
-	return false;
+  return false;
 }
-
-#endif	  // FILE_PATH_FILTER_H
