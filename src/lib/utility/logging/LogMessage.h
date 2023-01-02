08be089d3fd2f277e1877c581aa567c4adfcbde4
Ahmed Abdel Aal <eng.ahmedhussein89@gmail.com>
2023-01-02 15:47:02 +0200

    src: Remove warning from LogMessage
    ticket: SOUR-15

--- src/lib/utility/logging/LogMessage.h
+++ src/lib/utility/logging/LogMessage.h
@@ -1,47 +1,45 @@
-#ifndef LOG_MESSAGE_H
-#define LOG_MESSAGE_H
-
+#pragma once
+// STL
 #include <ctime>
 #include <string>
 #include <thread>
 
-struct LogMessage
-{
+struct LogMessage final {
 public:
-	LogMessage(
-		const std::wstring& message,
-		const std::string& filePath,
-		const std::string& functionName,
-		const unsigned int line,
-		const std::tm& time,
-		const std::thread::id& threadId)
-		: message(message)
-		, filePath(filePath)
-		, functionName(functionName)
-		, line(line)
-		, time(time)
-		, threadId(threadId)
-	{
-	}
+  LogMessage(const std::wstring& message_,
+             const std::string& filePath_,
+             const std::string& functionName_,
+             const unsigned int line_,
+             const std::tm& time_,
+             const std::thread::id& threadId_)
+      : message(message_)
+      , filePath(filePath_)
+      , functionName(functionName_)
+      , line(line_)
+      , time(time_)
+      , threadId(threadId_) {}
 
-	std::string getTimeString(const std::string& format) const
-	{
-		char timeString[50];
-		strftime(timeString, 50, format.c_str(), &time);
-		return std::string(timeString);
-	}
+  [[nodiscard]] std::string getTimeString(const std::string& format) const {
+    char timeString[50];
+#ifndef _WIN32
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wformat-nonliteral"
+#endif
+    strftime(timeString, 50, format.c_str(), &time);
+#ifndef _WIN32
+#pragma GCC diagnostic pop
+#endif
+    return timeString;
+  }
 
-	std::string getFileName() const
-	{
-		return filePath.substr(filePath.find_last_of("/\\") + 1);
-	}
+  [[nodiscard]] std::string getFileName() const {
+    return filePath.substr(filePath.find_last_of("/\\") + 1);
+  }
 
-	const std::wstring message;
-	const std::string filePath;
-	const std::string functionName;
-	const unsigned int line;
-	const std::tm time;
-	const std::thread::id threadId;
+  const std::wstring message;
+  const std::string filePath;
+  const std::string functionName;
+  const unsigned int line;
+  const std::tm time;
+  const std::thread::id threadId;
 };
-
-#endif	  // LOG_MESSAGE_H
