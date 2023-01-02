0baf4edc21b500f2b4d25e7add59c934f9f8869a
Ahmed Abdel Aal <eng.ahmedhussein89@gmail.com>
2023-01-02 18:53:45 +0200

    test: Add a test for utility::digits
    ticket: SOUR-15

--- src/test/UtilityTestSuite.cpp
+++ src/test/UtilityTestSuite.cpp
@@ -1,13 +1,11 @@
+// catch
 #include "catch.hpp"
-
+// internal
 #include "utility.h"
 
-TEST_CASE("trim blank spaces of string")
-{
-	REQUIRE(utility::trim(" foo  ") == "foo");
-}
-
-TEST_CASE("trim blank spaces of wstring")
-{
-	REQUIRE(utility::trim(L" foo  ") == L"foo");
+TEST_CASE("digits", "[utility]") {
+  REQUIRE(utility::digits(0)    == 1);
+  REQUIRE(utility::digits(10)   == 2);
+  REQUIRE(utility::digits(100)  == 3);
+  REQUIRE(utility::digits(1000) == 4);
 }
